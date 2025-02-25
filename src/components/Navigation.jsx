f1318dd5a180b271909f4a5dce63a4ae5f83634e
Zhivko Shopov <zhishopov@gmail.com>
2025-02-25 22:54:12 +0000

    Setup basic navigation

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+export default function Navigation() {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Menu</a>
+            </li>
+            <li>
+              <a href="/">Register</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
