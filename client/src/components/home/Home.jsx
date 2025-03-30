fe5f3d154b1c55913d0d9dab1c2222e9d2c7061e
Zhivko Shopov <zhishopov@gmail.com>
2025-03-30 23:34:38 +0100

    Change button position

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -2,8 +2,21 @@ import "./Home.css";
 
 export default function Home() {
   return (
-    <div className="main-content">
-      <button className="browse">Browse</button>
-    </div>
+    <>
+      <div className="main-content">
+        <button className="browse">Book a Table</button>
+      </div>
+      <div className="top-three">
+        <div className="product-box">
+          <h3>Prodcut 1</h3>
+        </div>
+        <div className="product-box">
+          <h3>Prodcut 2</h3>
+        </div>
+        <div className="product-box">
+          <h3>Prodcut 3</h3>
+        </div>
+      </div>
+    </>
   );
 }
