9d4a30e5a1454c264a78f182cab90854416e8eb7
Zhivko Shopov <zhishopov@gmail.com>
2025-03-30 23:45:55 +0100

    Add login page

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,3 +1,24 @@
+import "./login.css";
+
 export default function Login() {
-  return <h1>Login Page</h1>;
+  return (
+    <form id="login-form">
+      <h2>Login</h2>
+      <div>
+        <label htmlFor="email">
+          Email:
+          <input type="email" id="email" name="email" />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="password">
+          Password:
+          <input type="password" id="password" name="password" />
+        </label>
+      </div>
+      <div>
+        <button id="login-btn">Login</button>
+      </div>
+    </form>
+  );
 }
