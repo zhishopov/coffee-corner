9977b12edda454fecd805b3febd2ca8286c01a17
Zhivko Shopov <zhishopov@gmail.com>
2025-03-31 21:17:27 +0100

    Fix requester

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -1,47 +1,54 @@
 const request = async (method, url, data, options = {}) => {
-  if (method !== "GET") {
-    options.method = method;
+  try {
+    if (method !== "GET") {
+      options.method = method;
+    }
+
+    const authData = JSON.parse(localStorage.getItem("auth"));
+    if (authData?.accessToken) {
+      options = {
+        ...options,
+        headers: {
+          "X-Authorization": authData.accessToken,
+          ...options.headers,
+        },
+      };
+    }
+
+    if (data) {
+      options = {
+        ...options,
+        headers: {
+          "Content-Type": "application/json",
+          ...options.headers,
+        },
+        body: JSON.stringify(data),
+      };
+    }
+
+    const response = await fetch(url, options);
+    const responseContentType = response.headers.get("Content-Type");
+
+    if (!responseContentType) {
+      throw new Error("Invalid response: No content type");
+    }
+
+    let result;
+    try {
+      result = await response.json();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+
+    if (!response.ok) {
+      throw new Error(result.message || "Request failed");
+    }
+
+    return result;
+  } catch (error) {
+    console.error("Request error:", error.message);
+    throw error;
   }
-
-  // const authData = JSON.parse(localStorage.getItem('auth'));
-
-  // if (authData.accessToken) {
-  //     options = {
-  //         ...options,
-  //         headers: {
-  //             'X-Authorization': authData.accessToken,
-  //             ...options.headers,
-  //         },
-  //     }
-  // }
-
-  if (data) {
-    options = {
-      ...options,
-      headers: {
-        "Content-Type": "application/json",
-        ...options.headers,
-      },
-      body: JSON.stringify(data),
-    };
-  }
-
-  const response = await fetch(url, options);
-
-  const responseContentType = response.headers.get("Content-Type");
-  if (!responseContentType) {
-    return;
-  }
-
-  if (!response.ok) {
-    const result = await response.json();
-
-    throw result;
-  }
-
-  const result = await response.json();
-
-  return result;
 };
 
 export default {
