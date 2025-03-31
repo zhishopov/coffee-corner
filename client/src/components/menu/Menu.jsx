b6560d7a6a0e5d581128dd219f9a0e73e8368faf
Zhivko Shopov <zhishopov@gmail.com>
2025-03-31 20:02:34 +0100

    Improve and style menu page

--- client/src/components/menu/Menu.jsx
+++ client/src/components/menu/Menu.jsx
@@ -1,31 +1,33 @@
 import { useEffect, useState } from "react";
 import menuService from "../../services/menuService.js";
-
 import "./Menu.css";
 
 export default function Menu() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    try {
-      menuService.getAllProducts().then((result) => {
-        setProducts(result.products);
-      });
-    } catch (error) {
-      console.log(error.meesage);
-    }
+    menuService
+      .getAllProducts()
+      .then((result) => setProducts(result.products))
+      .catch((error) => console.error(error.message));
   }, []);
 
   return (
-    <div>
-      {products.map((product) => (
-        <>
-          <h1>Product</h1>
-          <p>Name: {product.name}</p>
-          <p>Description: {product.description}</p>
-          <p>Price: </p>
-          <img src={product.image} alt="Coffee" id="coffee" />
-        </>
+    <div className="menu-container">
+      {products.map((product, index) => (
+        <div key={index} className="product-card">
+          <img
+            src={product.image}
+            alt={product.name}
+            className="product-image"
+          />
+          <div className="product-info">
+            <h2>{product.name}</h2>
+            <p className="description">{product.description}</p>
+            <p className="price">£{product.price}</p>
+            <button>Details</button>
+          </div>
+        </div>
       ))}
     </div>
   );
