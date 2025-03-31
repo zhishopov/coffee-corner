425598ade68e9a4b07876209897ed4eada58ac0d
Zhivko Shopov <zhishopov@gmail.com>
2025-03-31 21:21:47 +0100

    Fix UserContext

--- client/src/contexts/UserContext.jsx
+++ client/src/contexts/UserContext.jsx
@@ -5,12 +5,20 @@ export const UserContext = createContext({
   email: "",
   username: "",
   accessToken: "",
-  userLoginHandler: () => null,
-  userLogoutHandler: () => null,
+  userLoginHandler: () => {
+    console.warn("userLoginHandler called outside of UserProvider.");
+  },
+  userLogoutHandler: () => {
+    console.warn("userLogoutHandler called outside of UserProvider.");
+  },
 });
 
 export function useUserContext() {
   const data = useContext(UserContext);
+
+  if (!data) {
+    console.error("useUserContext must be used within a UserProvider.");
+  }
 
   return data;
 }
