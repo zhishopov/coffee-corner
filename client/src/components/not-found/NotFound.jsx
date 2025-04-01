a6a959a9ae96d4a05852e5cd6badaa9b7b0e0998
Zhivko Shopov <zhishopov@gmail.com>
2025-04-01 13:47:55 +0100

    Add Not Found page

--- client/src/components/not-found/NotFound.jsx
+++ client/src/components/not-found/NotFound.jsx
@@ -3,7 +3,7 @@ import "./NotFound.css";
 
 export default function NotFound() {
   return (
-    <div className="notfound">
+    <div className="not-found">
       <h1>404 - Page Not Found</h1>
       <p>Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
       <Link to="/">Go back Home</Link>
