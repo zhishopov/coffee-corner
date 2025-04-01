c48972d368454dd6b7e8ab82bc664cc86bff78f9
Zhivko Shopov <zhishopov@gmail.com>
2025-04-01 13:09:55 +0100

    Make responsive header

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,3 +1,42 @@
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+import "./Footer.css";
+
 export default function Footer() {
-  return <p>TheCoffeeCorner@co.uk</p>;
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <p className="footer-text">TheCoffeeCorner@co.uk</p>
+        <div className="social-icons">
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook />
+          </a>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter />
+          </a>
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href="https://www.linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
